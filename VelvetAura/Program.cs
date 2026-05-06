374228d353221755ccad7f128d1e888fedc5f4b0
Nikol M <nikol.mosquera@upb.edu.co>
2026-05-06 16:43:20 -0500

    Eventos funcionales: PedidoCreado, EstadoCambiado, StockActualizado

--- VelvetAura/Program.cs
+++ VelvetAura/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace VelvetAura
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿using VelvetAura.Domain;
+using VelvetAura.Events;
+
+var cliente = new Cliente(1, "Luna", "luna@velvetaura.com");
+var pedido = new Pedido(cliente);
+pedido.PedidoCreado += (sender, e) => Console.WriteLine($"[EVENTO] {e.Mensaje}");
+pedido.EstadoPedidoCambiado += (sender, e) => Console.WriteLine($"[EVENTO] {e.Mensaje}");
+
+pedido.AgregarProducto(new Producto(101, "Perfume Aura", 120m, 5, "Perfumes"));
+pedido.CambiarEstado("Pagado");
+Console.WriteLine($"Total pedido: {pedido.Total:C}");
+
+var inventario = new Inventario();
+inventario.StockActualizado += (sender, e) => Console.WriteLine($"[EVENTO] {e.Mensaje}");
+var labial = new Producto(102, "Labial Rosa", 25m, 10, "Maquillaje");
+inventario.AgregarProducto(labial);
+inventario.ReducirStock(102, 2);
